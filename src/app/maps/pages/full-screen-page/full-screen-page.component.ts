f78b91fb6c0101cb228f35709b7c98f567073837
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-12-12 17:26:03 +0100

    updating

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWRwdGNvZGUiLCJhIjoiY20yajNyM2wxMDFoaDJqc2I4dG5keXAzaCJ9.qIRLrPbj_pGnE0QzjbwkUw';
@@ -11,9 +11,15 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWRwdGNvZGUiLCJhIjoiY20yajNyM2wxMDFoaDJqc2I4d
   styleUrl: './full-screen-page.component.css'
 })
 export class FullScreenPageComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap?: ElementRef;
+
   ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw new Error('The divMap element is not found');
+
     const map = new mapboxgl.Map({
-      container: 'map', // container ID
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRwdGNvZGUiLCJhIjoiY20yajNyM2wxMDFoaDJqc2I4dG5keXAzaCJ9.qIRLrPbj_pGnE0QzjbwkUw';
 
 @Component({
   selector: 'app-zoom-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './zoom-page.component.html',
   styleUrl: './zoom-page.component.css'
 })
-export class ZoomPageComponent {
+export class ZoomPageComponent implements AfterViewInit {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: mapboxgl.Map;
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw new Error('The divMap element is not found');
+
+    const map = new mapboxgl.Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+    this.mapListeners();
+  }
+
+  mapListeners() {
+     if ( !this.map ) throw new Error('The map is not found');
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+  }
 }
+
